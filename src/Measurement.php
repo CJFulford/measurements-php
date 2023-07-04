058507c7487cb651c0df15f3546b6cae11acf408
Cody Fulford <cody@codyfulford.com>
2023-07-04 13:45:15 -0600

    Add measurement addition

--- src/Measurement.php
+++ src/Measurement.php
@@ -36,10 +36,10 @@ abstract class Measurement
      * @return float
      * @throws Exception
      */
-    final public function getValue(Unit $unit): float
+    final public function getValue(Unit $unit, $precision = 5): float
     {
         $this->checkUnitCompatibility($unit);
-        return $this->value / $unit->getBaseUnitsPer();
+        return round($this->value / $unit->getBaseUnitsPer(), $precision);
     }
 
     final public function isMutable(): bool
@@ -69,5 +69,59 @@ abstract class Measurement
     {
         $this->isMutable = false;
         return $this;
+    }
+
+    /**
+     * determine if this is equal to another measurement
+     *
+     * @param Measurement|float $value if this value is a Length, $unit is ignored
+     * @param Unit|int|null $unit must be set if $value is a Length
+     * @param int $precision how many decimals to compare to
+     * @return bool
+     * @throws Exception
+     */
+    public function equals(Measurement|float $value, Unit|int|null $unit = null, int $precision = 5): bool
+    {
+        // add the value of the incoming measurement to this object
+        if ($value instanceof static) {
+            $thisValue = round($this->value, $precision);
+            $thatValue = round($value->value, $precision);
+            return $thisValue === $thatValue;
+        }
+
+        if ($unit === null) {
+            throw new Exception('No unit provided');
+        }
+
+        // ensure that $unit is a LengthUnit
+        $unit = $unit instanceof $this->unitClass ? $unit : new $this->unitClass($unit);
+        // recurse on this function now that the argument is a Length
+        return $this->equals(new static($value, $unit));
+    }
+
+    /**
+     * adds the provided length to this length
+     *
+     * @param Measurement|float $value if this value is a Length, $unit is ignored
+     * @param Unit|int|null $unit must be set if $value is a Length
+     * @return $this
+     * @throws Exception
+     */
+    public function add(Measurement|float $value, Unit|int|null $unit = null): self
+    {
+        // add the value of the incoming measurement to this object
+        if ($value instanceof static) {
+            $this->value += $value->value;
+            return $this;
+        }
+
+        if ($unit === null) {
+            throw new Exception('No unit provided');
+        }
+
+        // ensure that $unit is a LengthUnit
+        $unit = $unit instanceof $this->unitClass ? $unit : new $this->unitClass($unit);
+        // recurse on this function now that the argument is a Length
+        return $this->add(new static($value, $unit));
     }
 }
--- tests/LengthTest.php
+++ tests/LengthTest.php
@@ -7,22 +7,76 @@ test('Invalid Length Creation', function () {
     new Length(1, 0);
 })->throws(Exception::class);
 
-$defaultValue = 10.0;
+function getRandom(int $precision): float
+{
+    return round(random_int(-1000, 1000) / random_int(-1000, 1000), $precision);
+}
+
 
 foreach (array_keys(LengthUnit::getUnitDefinitions()) as $unitId) {
     $unit = new LengthUnit($unitId);
 
-    test('Creation of lengths for ' . $unit->getName(), function () use ($defaultValue, $unitId, $unit) {
+    $precision    = random_int(1, 8);
+    $defaultValue = getRandom($precision);
+
+    test("{$unit->getName()} - Create length from ID", function () use ($defaultValue, $unitId) {
         // create from ID
         $length = new Length($defaultValue, $unitId);
         expect($length)->toBeInstanceOf(Length::class);
+    });
 
-        // create from object
-        $unit   = new LengthUnit($unitId);
+    test("{$unit->getName()} - Create length from Object", function () use ($defaultValue, $unit) {
         $length = new Length($defaultValue, $unit);
         expect($length)->toBeInstanceOf(Length::class);
+    });
 
-        // after creation, getting the original value in the original units is
-        expect($length->getValue($unit))->toBe($defaultValue);
+    test(
+        "{$unit->getName()} - Get original value in original units",
+        function () use ($defaultValue, $unit, $precision) {
+            $length = new Length($defaultValue, $unit);
+            expect($length->getValue($unit, $precision))->toBe(round($defaultValue, $precision));
+        }
+    );
+
+    test(
+        "{$unit->getName()} - Compare equality of length to length",
+        function () use ($defaultValue, $unit, $precision) {
+            $length1 = new Length($defaultValue, $unit);
+            $length2 = new Length($defaultValue, $unit);
+            expect($length1->equals($length2, $precision))->toBeTrue();
+        }
+    );
+
+    test(
+        "{$unit->getName()} - Compare equality of length to length arguments",
+        function () use ($defaultValue, $unit, $precision) {
+            $length1 = new Length($defaultValue, $unit);
+            expect($length1->equals($defaultValue, $unit->getId(), $precision))->toBeTrue();
+        }
+    );
+
+    test("{$unit->getName()} - Add 2 lengths", function () use ($unit, $precision) {
+        $value1      = getRandom($precision);
+        $value2      = getRandom($precision);
+        $resultValue = round($value1 + $value2, $precision);
+
+        $length1 = new Length($value1, $unit);
+        $length2 = new Length($value2, $unit);
+
+        $length1->add($length2);
+
+        expect($length1->equals($resultValue, $unit, $precision))->toBeTrue();
+    });
+
+    test("{$unit->getName()} - Add length parameters", function () use ($unit, $precision) {
+        $value1      = getRandom($precision);
+        $value2      = getRandom($precision);
+        $resultValue = round($value1 + $value2, $precision);
+
+        $length1 = new Length($value1, $unit);
+
+        $length1->add($value2, $unit);
+
+        expect($length1->equals($resultValue, $unit, $precision))->toBeTrue();
     });
 }
