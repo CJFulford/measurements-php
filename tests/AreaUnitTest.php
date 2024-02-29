f76907c384d76fbdfdc6b28e512098f76f0c2d5a
Cody Fulford <cody.f@integritybuilt.com>
2024-02-29 07:45:47 -0700

    Add tests for custom unit creation

--- tests/AreaUnitTest.php
+++ tests/AreaUnitTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Cjfulford\Measurements\AreaUnit;
+use Cjfulford\Measurements\LengthUnit;
 use PHPUnit\Framework\TestCase;
 
 final class AreaUnitTest extends TestCase
@@ -44,6 +45,98 @@ final class AreaUnitTest extends TestCase
         $this->assertSame(
             $first,
             AreaUnit::getBySymbol($first->symbol)
+        );
+    }
+
+    public function testCustomUnitCreation(): void
+    {
+        $lengthUnit1 = new LengthUnit(
+            id          : 20,
+            baseUnitsPer: 40,
+            name        : 'test1',
+            pluralName  : 'test1s',
+            acronym     : 't1',
+            symbol      : 't1'
+        );
+        $lengthUnit2 = new LengthUnit(
+            id          : 21,
+            baseUnitsPer: 41,
+            name        : 'test2',
+            pluralName  : 'test2s',
+            acronym     : 't2',
+            symbol      : 't2'
+        );
+        $lengthUnit3 = new LengthUnit(
+            id          : 22,
+            baseUnitsPer: 42,
+            name        : 'test3',
+            pluralName  : 'test3s',
+            acronym     : 't3',
+            symbol      : 't3'
+        );
+
+        $unit1 = new AreaUnit(
+            id                       : 20,
+            name                     : 'test1',
+            pluralName               : 'test1s',
+            acronym                  : 't1',
+            correspondingLengthUnitId: $lengthUnit1->id
+        );
+        $this->assertSame($unit1, AreaUnit::getById(20));
+        $this->assertSame($unit1, AreaUnit::getByAcronym('t1'));
+        $this->assertSame($unit1, AreaUnit::getBySymbol('t1'));
+
+        $unit2 = new AreaUnit(
+            id                       : 21,
+            name                     : 'test2',
+            pluralName               : 'test2s',
+            acronym                  : 't2',
+            correspondingLengthUnitId: $lengthUnit2->id
+        );
+        $this->assertSame($unit2, AreaUnit::getById(21));
+        $this->assertSame($unit2, AreaUnit::getByAcronym('t2'));
+        $this->assertSame($unit2, AreaUnit::getBySymbol('t2'));
+
+        // test that adding a duplicate in any way fails
+        $this->expectException(InvalidArgumentException::class);
+        new AreaUnit(
+            id                       : 20,
+            name                     : 'test3',
+            pluralName               : 'test3s',
+            acronym                  : 't3',
+            correspondingLengthUnitId: $lengthUnit3->id
+        );
+        $this->expectException(InvalidArgumentException::class);
+        new AreaUnit(
+            id                       : 23,
+            name                     : 'test1',
+            pluralName               : 'test3s',
+            acronym                  : 't3',
+            correspondingLengthUnitId: $lengthUnit3->id
+        );
+        $this->expectException(InvalidArgumentException::class);
+        new AreaUnit(
+            id                       : 23,
+            name                     : 'test3',
+            pluralName               : 'test1s',
+            acronym                  : 't3',
+            correspondingLengthUnitId: $lengthUnit3->id
+        );
+        $this->expectException(InvalidArgumentException::class);
+        new AreaUnit(
+            id                       : 23,
+            name                     : 'test3',
+            pluralName               : 'test3s',
+            acronym                  : 't1',
+            correspondingLengthUnitId: $lengthUnit3->id
+        );
+        $this->expectException(InvalidArgumentException::class);
+        new AreaUnit(
+            id                       : 23,
+            name                     : 'test3',
+            pluralName               : 'test3s',
+            acronym                  : 't3',
+            correspondingLengthUnitId: $lengthUnit1->id
         );
     }
 }
