fad37fe13364958ee682430a7538f85b334f33aa
cody-fulford <cody@codyfulford.com>
2025-04-16 10:40:08 -0600

    Add floatsGreaterThan and floatsLessThan helpers

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,6 +4,21 @@ namespace Cjfulford\Measurements\Helpers;
 
 function floatsEqual(float $a, float $b, int $toNumberOfDecimals = 5): bool
 {
-    $epsilon = $toNumberOfDecimals === 5 ? 0.000001 : 0.1 / pow(10, $toNumberOfDecimals);
-    return abs($a - $b) < $epsilon;
+    $a = round($a, $toNumberOfDecimals);
+    $b = round($b, $toNumberOfDecimals);
+    return $a === $b;
+}
+
+function floatsGreaterThan(float $a, float $b, int $toNumberOfDecimals = 5): bool
+{
+    $a = round($a, $toNumberOfDecimals);
+    $b = round($b, $toNumberOfDecimals);
+    return $a > $b;
+}
+
+function floatsLessThan(float $a, float $b, int $toNumberOfDecimals = 5): bool
+{
+    $a = round($a, $toNumberOfDecimals);
+    $b = round($b, $toNumberOfDecimals);
+    return $a < $b;
 }
